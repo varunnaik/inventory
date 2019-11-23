fe41c286ad8e2b3ef9a4418974bb104227c178e4
Varun Naik <varun@vnaik.com>
2019-11-23 16:15:48 +1100

    Add enntities for Asset and ShoppingCentre

---
+++ src/entity/ShoppingCentre.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, OneToMany, Column } from "typeorm";
+import { Asset } from "./Asset";
+
+@Entity()
+export class ShoppingCentre {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  address: string;
+
+  @OneToMany(
+    (type: any) => Asset,
+    (asset: Asset) => asset.shoppingcentre
+  )
+  assets: Asset[];
+
+  @Column()
+  created_at: number;
+}
