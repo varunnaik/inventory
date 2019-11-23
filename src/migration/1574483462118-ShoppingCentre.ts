028105677a2332742c6e62af77063403ae53f664
Varun Naik <varun@vnaik.com>
2019-11-23 16:16:17 +1100

    Add migrations for database tabes

---
+++ src/migration/1574483462118-ShoppingCentre.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class ShoppingCentre1574483462118 implements MigrationInterface {
+  name = "ShoppingCentre1574483462118";
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `CREATE TABLE "shopping_centre" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "address" character varying NOT NULL, "created_at" integer NOT NULL, CONSTRAINT "PK_2562a60f20b71a0310c7323395a" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
+      `CREATE TABLE "asset" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "active" boolean NOT NULL, "name" character varying NOT NULL, "dimensions" json NOT NULL, "location" json NOT NULL, "status" character varying NOT NULL, "created_at" integer NOT NULL, "shoppingcentreId" uuid, CONSTRAINT "PK_1209d107fe21482beaea51b745e" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "asset" ADD CONSTRAINT "FK_f98956c91151e34b45ab207542f" FOREIGN KEY ("shoppingcentreId") REFERENCES "shopping_centre"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `ALTER TABLE "asset" DROP CONSTRAINT "FK_f98956c91151e34b45ab207542f"`
+    );
+    await queryRunner.query(`DROP TABLE "asset"`);
+    await queryRunner.query(`DROP TABLE "shopping_centre"`);
+  }
+}
