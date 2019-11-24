99a3e80afde7eb251a7d493caf90b10dce6f6648
Varun Naik <varun@vnaik.com>
2019-11-24 12:52:40 +1100

    Hook routes up to controllers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
-import http from "http";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 import express from "express";
+import * as bodyParser from "body-parser";
+import compression from "compression";
+import cors from "cors";
+import { handleApiErrors } from "./utils/handleApiErrors";
 
-const router = express();
+import ShoppingCentreController from "./controllers/ShoppingCentreController";
+import AssetController from "./controllers/AssetController";
+
 const { PORT = 3000 } = process.env;
-const server = http.createServer(router);
- 
-server.listen(PORT, () =>
-  console.log(`Server is running http://localhost:${PORT}...`)
-);
+
+const app = () => {
+  createConnection()
+    .then(async connection => {
+      const app = express();
+      app.disable("x-powered-by");
+
+      app.use(cors());
+      app.use(compression());
+      app.use(bodyParser.json());
+
+      app.get("/", (req, res) => res.json({ name: "Inventory API" }));
+
+      app.use("/asset", AssetController());
+      app.use("/shopping-centre", ShoppingCentreController());
+
+      app.use(handleApiErrors);
+      app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+      });
+    })
+    .catch(error => console.log(error));
+};
+
+app();
