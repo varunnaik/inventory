f11b3ac7d368d887e97ad0f5422df9c67163e582
Varun Naik <varun@vnaik.com>
2019-11-24 12:49:06 +1100

    Update migration to use current time for created at as default

--- src/migration/1574483462118-ShoppingCentre.ts
+++ src/migration/1574483462118-ShoppingCentre.ts
@@ -5,10 +5,10 @@ export class ShoppingCentre1574483462118 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      `CREATE TABLE "shopping_centre" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "address" character varying NOT NULL, "created_at" integer NOT NULL, CONSTRAINT "PK_2562a60f20b71a0310c7323395a" PRIMARY KEY ("id"))`
+      `CREATE TABLE "shopping_centre" ("id" uuid NOT NULL, "name" character varying NOT NULL, "address" character varying NOT NULL, "created_at" integer NOT NULL DEFAULT CURRENT TIMESTAMP, CONSTRAINT "PK_2562a60f20b71a0310c7323395a" PRIMARY KEY ("id"))`
     );
     await queryRunner.query(
-      `CREATE TABLE "asset" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "active" boolean NOT NULL, "name" character varying NOT NULL, "dimensions" json NOT NULL, "location" json NOT NULL, "status" character varying NOT NULL, "created_at" integer NOT NULL, "shoppingcentreId" uuid, CONSTRAINT "PK_1209d107fe21482beaea51b745e" PRIMARY KEY ("id"))`
+      `CREATE TABLE "asset" ("id" uuid NOT NULL, "active" boolean NOT NULL, "name" character varying NOT NULL, "dimensions" json NOT NULL, "location" json NOT NULL, "status" character varying NOT NULL, "created_at" integer NOT NULL DEFAULT CURRENT TIMESTAMP, "shoppingcentreId" uuid, CONSTRAINT "PK_1209d107fe21482beaea51b745e" PRIMARY KEY ("id"))`
     );
     await queryRunner.query(
       `ALTER TABLE "asset" ADD CONSTRAINT "FK_f98956c91151e34b45ab207542f" FOREIGN KEY ("shoppingcentreId") REFERENCES "shopping_centre"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
