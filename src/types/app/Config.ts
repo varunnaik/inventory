3ac8fd2cb3cb6a26baa5be94677f550ecfe14ec7
Varun Naik <varun@vnaik.com>
2019-11-26 08:00:31 +1100

    Add server port to config

--- src/types/app/Config.ts
+++ src/types/app/Config.ts
@@ -1,15 +1,18 @@
 import { ConnectionOptions } from "typeorm";
 export interface ConfigType {
   jwtSecret: string;
+  port: number;
   database: ConnectionOptions;
 }
 
 export class Config implements ConfigType {
   public jwtSecret: string;
+  public port: number;
   public database: any;
 
   constructor(env: any) {
     this.jwtSecret = env.JWT_SECRET;
+    this.port = env.PORT;
     this.database = <ConnectionOptions>{
       type: "postgres",
       host: env.PG_HOST,
